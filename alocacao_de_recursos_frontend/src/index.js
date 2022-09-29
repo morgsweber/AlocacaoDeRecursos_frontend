d4890c1b59c9f687fca337f6d4e55a4a2113f68d
kazz <kazztt@hotmail.com>
2022-09-29 21:39:30 +0000

    fixed files hierarchy

--- alocacao_de_recursos_frontend/src/index.js
+++ alocacao_de_recursos_frontend/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import LoginButton from './components/login-button';
-import LogoutButton from './components/logout-button';
-//import Profile from './components/profile';
-import AppLogo from './components/app-logo';
+
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 
@@ -16,15 +14,7 @@ root.render(
     clientId={"1atUjfY2HNIL4swEXvG8Iqjur7NI0pHO"}
     redirectUri={window.location.origin}
   >
-    <div class="container">
-    
-      <div>
-        <AppLogo/>
-        <LoginButton />
-        <LogoutButton />
-      </div>
-    </div>
-
+    <App/>
   </Auth0Provider>
 );
 
