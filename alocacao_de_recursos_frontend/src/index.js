5bea92884b8504e4a597cfa6dceb91574bc653c6
notkazz <Kazztt@hotmail.com>
2022-10-18 10:48:14 -0300

    removed web vitals

--- alocacao_de_recursos_frontend/src/index.js
+++ alocacao_de_recursos_frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -21,8 +20,3 @@ root.render(
     </Auth0Provider>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
