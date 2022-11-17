9accf011e2c741a388c4b02ff92e777d82f0b818
Morgana Weber <50406261+morgsweber@users.noreply.github.com>
2022-11-17 18:42:32 -0300

    ✨ Tela Professor horários de aula

--- alocacao_de_recursos_frontend/src/pages/professorHorarios.js
+++ alocacao_de_recursos_frontend/src/pages/professorHorarios.js
@@ -4,9 +4,16 @@ import Loading from "./loading";
 import Modal from "./modais/Modal";
 import Sidebar from "./components/sidebar"
 import "./pages_css/base.css";
+import "./pages_css/professorHorarios.css"
 
 function ProfessorHorarios() {
   const [show, setShow] = useState(false)
+
+  const data = [
+    { data: "02/08/2022", descricao: "Apresentação disciplina", local: "32/315" },
+    { data: "04/08/2022", descricao: "Desenvolvimento", local: "32/315" },
+    { data: "09/08/2022", descricao: "Desenvolvimento", local: "32/315" },
+  ]
 
   return (
     <div>
@@ -14,14 +21,30 @@ function ProfessorHorarios() {
 
       <div class="main">
         <div class="header">
-          <p>Grade Horária</p>
+          Horários
         </div>
+        <div>
+          <table class="table">
+            <tr>
+              <th>Data</th>
+              <th>Descrição da aula</th>
+              <th>Prédio/sala</th>
+              <th></th>
+            </tr>
+            {data.map((val, key) => {
+              return (
+                <tr key={key}>
+                  <td>{val.data}</td>
+                  <td>{val.descricao}</td>
+                  <td>{val.local}</td>
+                  <td><button onClick={() => setShow(true)}> Solicitar troca </button></td>
+                </tr>
+              )
+            })}
+          </table>
 
-        <div class="ProfessorHorarios">
-          <h2>ProfessorHorarios</h2>
-          <button onClick={() => setShow(true) }> Show modal </button>
         </div>
-        <Modal onClose = {() => setShow(false)} show={show} />
+        <Modal onClose={() => setShow(false)} show={show} />
       </div>
     </div>
   );
